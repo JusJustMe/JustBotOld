c15cb8b17ed30913bbca49d230594a2f75ab68f4
mini500 <Tommy.ohrberg3@gmail.com>
2017-08-09 00:22:39 +0200

    mini500
    h

--- basicBot.js
+++ basicBot.js
@@ -1,3 +1,12 @@
+/**
+
+ Copyright © 2014-2017 basicBot
+
+ Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
+ This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
+
+ */
+
 (function() {
 
     /*window.onerror = function() {
@@ -233,8 +242,8 @@
     };
 
     var botCreator = 'mini500';
-    var botMaintainer = 'JustJustme'
-    var botCreatorIDs = ['3851534', '16498004'];
+    var botMaintainer = 'JustJustMe'
+    var botCreatorIDs = ['4878561', '16498004'];
 
     var basicBot = {
         version: '0.1',
