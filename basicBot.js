e9fcedc0804a667f2bd8b36f62727204dfa20c8c
mini500 <Tommy.ohrberg3@gmail.com>
2017-08-08 23:26:04 +0200

    mini500
    fix

--- basicBot.js
+++ basicBot.js
@@ -1,12 +1,3 @@
-/**
-
- Copyright © 2014-2017 basicBot
-
- Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
- This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
-
- */
-
 (function() {
 
     /*window.onerror = function() {
@@ -250,7 +241,7 @@
         status: false,
         name: 'JustBot',
         loggedInID: null,
-        scriptLink: 'https://github.com/mini500/JustMiniBot/blob/Bot.js/basicBot.js',
+        scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
         cmdLink: 'https://github.com/mini500/JustMiniBot/blob/Bot.js/commands.md',
         chatLink: 'https://github.com/mini500/JustMiniBot/blob/Bot.js/lang/en.json',
         chat: null,
